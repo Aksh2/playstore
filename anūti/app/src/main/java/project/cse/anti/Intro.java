35069fd7fb635664e8a69e0267f519d7b10d16ee
akshay <akshay.h@games24x7.com>
2016-12-27 10:16:11 +0530

    Added showcase and appintro and fixed few bugs

---
+++ anūti/app/src/main/java/project/cse/anti/Intro.java
@@ -0,0 +1,14 @@
+package project.cse.anti;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Intro extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+
+}
